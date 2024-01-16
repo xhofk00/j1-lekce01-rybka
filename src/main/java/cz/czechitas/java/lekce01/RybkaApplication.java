36128207c58e80f4aa6c6faac95144e95c58bc87
hofrichterovak <hofrichterovak@gmail.com>
2024-01-16 19:56:26 +0100

    moving

--- src/main/java/cz/czechitas/java/lekce01/RybkaApplication.java
+++ src/main/java/cz/czechitas/java/lekce01/RybkaApplication.java
@@ -34,5 +34,10 @@ public class RybkaApplication extends CzechitasGameApplication {
   protected void onStart() {
     kamen = new Kamen(200, 200);
     rybka = new Rybka(100, 100);
+
+    rybka.plavatVpravo();
+    rybka.plavatDolu();
+    rybka.plavatVlevo();
+    rybka.plavatNahoru();
   }
 }
